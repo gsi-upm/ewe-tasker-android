a3fcf722a1de93e50dc7bcb96a28e1791002269c
afllamas <a.fllamas@alumnos.upm.es>
2016-10-24 12:21:55 +0200

    Gestor performer (testing)

---
+++ app/src/main/java/es/dit/gsi/rulesframework/GestorWebViewActivity.java
@@ -0,0 +1,30 @@
+package es.dit.gsi.rulesframework;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class GestorWebViewActivity extends AppCompatActivity {
+
+    WebView webView;
+    String url;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gestor_web_view);
+
+        url = getIntent().getExtras().getString("url");
+
+        webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient(){
+            public boolean shouldOverrideUrlLoading(WebView view,String url){
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        webView.loadUrl("http://www.google.es");
+    }
+}
